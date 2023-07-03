86641c596a0f1dbafbf39a8d2942fcc3822d0bc1
Nick Scofield <c.nick.scofield@gmail.com>
2023-07-03 01:54:14 -0600

    Complete Chapters 6, 7, &  8

--- notes/views.py
+++ notes/views.py
@@ -1,25 +1,65 @@
-from django.shortcuts import render
+from typing import Any
+from django.http.response import HttpResponseRedirect
+# Removed for testing
+# from django.shortcuts import render
 from django.http import Http404
-from django.views.generic import ListView, DetailView
+from django.views.generic import CreateView, DetailView, ListView, UpdateView
+from django.views.generic.edit import DeleteView
+from django.contrib.auth.mixins import LoginRequiredMixin
 
+from .forms import NotesForm
 from .models import Notes
 
 
+class NotesDeleteView(DeleteView):
+    model = Notes
+    success_url = '/smart/notes'
+    template_name = "notes/notes_delete.html"
+
+
+class NotesUpdateView(UpdateView):
+    model = Notes
+    success_url = '/smart/notes'
+    form_class = NotesForm
+
+
+class NotesCreateView(CreateView):
+    model = Notes
+    success_url = '/smart/notes'
+    form_class = NotesForm
+
+    # Overrides the Django native method form_valid
+    # Results in logged in user being attched to the new note
+    def form_valid(self, form):
+        self.object = form.save(commit=False)
+        self.object.user = self.request.user
+        self.object.save()
+        return HttpResponseRedirect(self.get_success_url())
+
+
 # Preferred class based view
-class NotesListView(ListView):
+class NotesListView(LoginRequiredMixin, ListView):
     model = Notes
     context_object_name = "notes"
     template_name = "notes/notes_list.html"
+    login_url = '/admin'
+
+    # Overrides the Django native method get_queryset
+    # Results in user only seeing notes for logged in user, not entire set
+    def get_queryset(self):
+        return self.request.user.notes.all()
+
+
+class NotesDetailView(DetailView):
+    model = Notes
+    context_object_name = "note"
+
 
 # Antiquated method
 # def list(request):
 #     all_notes = Notes.objects.all()
 #     return render(request, 'notes/notes_list.html', {'notes': all_notes})
 
-
-class NotesDetailView(DetailView):
-    model = Notes
-    context_object_name = "note"
 
 # Antiquated method
 # def detail(request, pk):
