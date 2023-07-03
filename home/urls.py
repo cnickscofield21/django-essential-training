4dac8a8aea027888cc97b9bfcf2fa0c0c38e7da5
Nick Scofield <c.nick.scofield@gmail.com>
2023-07-03 12:48:27 -0600

    Complete Chapter 9

--- home/urls.py
+++ home/urls.py
@@ -4,6 +4,11 @@ from . import views
 
 
 urlpatterns = [
-    path('home', views.HomeView.as_view()),
-    path('authorized', views.AuthorizedView.as_view()),
+    path('', views.HomeView.as_view(), name='home'),
+    path('login', views.LoginInterfaceView.as_view(), name='login'),
+    path('logout', views.LogoutInterfaceView.as_view(), name='logout'),
+    path('signup', views.SignupView.as_view(), name='signup'),
+
+    # No longer needed since login/logout has been implmented.
+    # path('authorized', views.AuthorizedView.as_view(), name='home.authorized'),
 ]
--- home/views.py
+++ home/views.py
@@ -1,9 +1,34 @@
-from django.shortcuts import render
-from django.http import HttpResponse
 from datetime import datetime
-from django.contrib.auth.decorators import login_required
 from django.views.generic import TemplateView
+from django.views.generic.edit import CreateView
 from django.contrib.auth.mixins import LoginRequiredMixin
+from django.contrib.auth.views import LoginView, LogoutView
+from django.contrib.auth.forms import UserCreationForm
+
+from django.shortcuts import redirect
+
+
+class SignupView(CreateView):
+    form_class = UserCreationForm
+    template_name = 'home/register.html'
+    success_url = '/smart/notes'
+
+    def get(self, request, *args, **kwargs):
+        # If user is already authorized, pass through to notes.list
+        if self.request.user.is_authenticated:
+            return redirect('notes.list')
+
+        # The replicates the original signin functionality, but blocks currently
+        # signed in users from creating another account
+        return super().get(request, *args, **kwargs)
+
+
+class LoginInterfaceView(LoginView):
+    template_name = 'home/login.html'
+
+
+class LogoutInterfaceView(LogoutView):
+    template_name = 'home/logout.html'
 
 
 # Preferred class based approach
@@ -11,16 +36,19 @@ class HomeView(TemplateView):
     template_name = 'home/welcome.html'
     extra_context = {'today': datetime.today()}
 
-# More primative method
-# def home(request):
-#     return render(request, 'home/welcome.html', {'today': datetime.today()})
-
 
 # Preferred class based approach
 # Note use of .mixins above
 class AuthorizedView(LoginRequiredMixin, TemplateView):
     template_name = 'home/authorized.html'
-    login_url = '/admin'
+    login_url = 'home/login.html'
+    # login_url = '/admin'
+
+
+# More primative method
+# def home(request):
+#     return render(request, 'home/welcome.html', {'today': datetime.today()})
+
 
 # More primative method
 # @login_required(login_url='/admin')
